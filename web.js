04dae83af2a7e64637599a2e2ff110fb222852cb
tmwilder <tmwilder@gmail.com>
2013-08-07 04:08:28 +0000

    Workflow test commit, remove silly sentence.

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ app.get('/', function(request, response) {
   response.send(html);
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
